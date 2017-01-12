3f77e0a68c35a1a9aec1665ceefb488abfd9103a
HyundongHwang <hhd2002@gmail.com>
2017-01-12 17:48:11 +0900

    modify

--- PsScripts/profile.ps1
+++ PsScripts/profile.ps1
@@ -915,6 +915,102 @@ function hhdgitposhinit
 
 
 
+<#
+.SYNOPSIS
+.EXAMPLE
+#>
+function hhdgitstashsave
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelinebyPropertyName=$true)]
+        [System.String]
+        $stashName
+    )
+
+    Read-Host "git status ..."
+    git status
+    
+    Read-Host "git stash list ..."
+    git stash list
+
+    Read-Host "git stash save -u `"$stashName`" ..."
+    git stash save -u "$stashName"
+}
+
+
+
+<#
+.SYNOPSIS
+.EXAMPLE
+#>
+function hhdgitstashapply
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelinebyPropertyName=$true)]
+        [System.Int32]
+        $stashNumber
+    )
+
+    Read-Host "git status ..."
+    git status
+    
+    Read-Host "git stash list ..."
+    git stash list
+
+    if ($stashName -eq "") 
+    {
+        Read-Host "git stash pop ..."
+        git stash pop
+    }
+    else 
+    {
+        Read-Host "git stash apply `"stash@{$stashNumber}`" ..."
+        git stash apply "stash@{$stashNumber}"
+    }
+}
+
+
+
+<#
+.SYNOPSIS
+.EXAMPLE
+#>
+function hhdgitaddcommitpush
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelinebyPropertyName=$true)]
+        [System.String]
+        $commitMsg
+    )
+
+    Read-Host "git status ..."
+    git status
+    
+    Read-Host "git add * ..."
+    git add *
+
+    if ($commitMsg -eq "") 
+    {
+        $commitMsg = "modify"
+    }
+
+    Read-Host "git commit -m `"$commitMsg`" ..."
+    git commit -m "$commitMsg"
+
+    Read-Host "git pull ..."
+    git pull
+
+    Read-Host "git push ..."
+    git push
+}
+
+
 
 
 
